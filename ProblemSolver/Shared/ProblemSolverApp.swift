820add7eb9f50df486bed71e7f1894db298f0bef
Mugish <mohammad.mugish@gmail.com>
2020-07-17 08:16:01 +0530

    ProjectCreated

---
+++ ProblemSolver/Shared/ProblemSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProblemSolverApp.swift
+//  Shared
+//
+//  Created by Mohd Yasir on 17/07/20.
+//
+
+import SwiftUI
+
+@main
+struct ProblemSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
